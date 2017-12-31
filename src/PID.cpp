46e5e1195488ba3201ced2331fc09ce738b554cd
Raahul Seshadri <raahul@musictypes.com>
2018-01-01 00:44:56 +0530

    Added parameters that work

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "PID.h"
 
 using namespace std;
@@ -11,11 +12,29 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    // Initialize parameters
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    // Initialize errors to be 0
+    last_cte = 0;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    i_error += Ki * cte;
+    p_error = Kp * cte;
+
+    double d_cte = cte - last_cte;
+    d_error = Kd * d_cte;
+
+    last_cte = cte;
 }
 
 double PID::TotalError() {
+    return p_error + i_error + d_error;
 }
 
